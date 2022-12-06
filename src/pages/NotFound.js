a8c401e240cce503b304ceb84e35b87b17787d78
BeatrizNataly <beatriznatyce@gmail.com>
2022-12-06 17:47:41 -0300

    layout adicionado

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,25 @@
 function NotFound() {
     return (
+        <>
+        <div style={{
+            backgroundColor: 'crimson'
+        }}><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
         <div style={{
             backgroundColor: 'lightcoral',
             textAlign: 'center'
         }}>
-            <h1>404 Página não encontrada</h1>
+            <h1>404</h1>
         </div>
+        <div style={{
+            backgroundColor: 'lightblue',
+            textAlign: 'center'
+        }}>
+            <h1>Página não encontrada</h1>
+        </div>
+        <div style={{
+            backgroundColor: 'crimson'
+        }}><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></div>
+        </>
     )
 }
 
